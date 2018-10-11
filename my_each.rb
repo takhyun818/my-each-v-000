8a447b2491157c2c414c5e9c8eb5b5ebf2fac8c8
Tak H Kim <takhyun818@gmail.com>
2018-10-11 21:03:03 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(word) # put argument(s) here
+   i = 0
+     while i < word.length
+     yield (word[i])
+     i += 1
+  # # code here
+  end
+  word
 end
